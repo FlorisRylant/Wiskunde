e1fae2e6cc3d64812d4e615a6bbddb9342fc5172
FlorisRylant <florisrylant@gmail.com>
2025-11-19 14:36:01 +0100

    Functies ingebouwd -> variabelenlijst aanwezig

--- functie_objects.py
+++ functie_objects.py
@@ -4,6 +4,12 @@ class Functie:
         if type(argumenten) != tuple:
             argumenten = tuple(argumenten)
         self.__args = argumenten
+        self.__onbekendes = set()
+        for arg in self.__args:
+            self.__onbekendes = self.__onbekendes.union(arg.get_onbekendes())
+
+    def get_onbekendes(self): # geeft een set met alle onbekendes in de functie
+        return self.__onbekendes
     
     def __str__(self):
         if self.__op in '+*^':
@@ -37,6 +43,9 @@ class Constante(Functie):
     
     def __repr__(self):
         return f"Constante({self.__val})"
+    
+    def get_onbekendes(self):
+        return set()
 
 
 class Onbekende(Functie):
@@ -51,6 +60,18 @@ class Onbekende(Functie):
     
     def __repr__(self):
         return f"Onbekende({self.__naam})"
+    
+    def get_naam(self):
+        return self.__naam
+    
+    def __eq__(self, other):
+        return self.__naam == other.get_naam()
+    
+    def __lt__(self, other):
+        return self.__naam < other.get_naam()
+    
+    def get_onbekendes(self): # om de onbekendes te laten opzalmen
+        return {self.__naam}
 
 
 def main():
@@ -59,6 +80,7 @@ def main():
     x = Onbekende('x')
     f = Functie('min', Functie('+', a, Functie('*', b, x)), a, b)
     print(f)
+    print(f.get_onbekendes())
 
 if __name__ == '__main__':
     main()
