e92cab08b0aac55e36d0fa9ebae207363402fb62
FlorisRylant <florisrylant@gmail.com>
2025-11-20 19:04:28 +0100

    Algoritme aangepast zodat haakjes correct zijn Opgeruimd

--- IOmaths/data.py
+++ IOmaths/data.py
@@ -9,11 +9,14 @@ functions = {'min':{'f':min, 'args':2},
              'tan':{'f':tan, 'args':1},
              'cot':{'f':cot, 'args':1},
              'sqrt':{'f':sqrt, 'args':1}} # f = verwijzing naar de functie, args = aantal argumenten
+
 special = {'(', ')', ','}
+
 operators = {'^':{'prec':4, 'LtoR':False},
              '*':{'prec':3, 'LtoR':True},
              '/':{'prec':3, 'LtoR':True},
              '+':{'prec':2, 'LtoR':True},
              '-':{'prec':2, 'LtoR':True},
              }
+
 aliases = {'**':'^', ';':',', '[':'(', ']':')', 'V':'sqrt'}
--- IOmaths/infix_to_tree.py
+++ IOmaths/infix_to_tree.py
@@ -29,13 +29,8 @@ def in_to_tree(infix): # infix = lijst met in volgorde de delen van de uitdrukki
     for op in '-+*/^': # maakt een lijst met alle operatoren in infix
         ops.extend([op for _ in range(infix.count(op))])
     
-    places = []
-    for i, op in enumerate(infix):
-        if op in operators.keys():
-            places.append(i)
-    offset = 0
-
     for op in ops: # 'bubbelt' elke operator een voor een naar de juiste plek
+        print(''.join([str(i) for i in infix]))
         l = infix.index(op) # startpositie van die operator
         infix[l] = ','
         r = l # rechter-'bubbel'
@@ -48,43 +43,43 @@ def in_to_tree(infix): # infix = lijst met in volgorde de delen van de uitdrukki
                 infix = [op+'$', '('] + infix
                 l_go = False
                 r += 2
-                places = pas_posities_aan(places, l, 2)
             elif str(infix[l]) in operators and operators[infix[l]]['prec'] <= operators[op]['prec']:
                 infix.insert(l+1, '(')
                 infix.insert(l+1, op+'$')
                 l_go = False
                 r += 2
-                places = pas_posities_aan(places, l, 2)
             elif infix[l] == '(':
-                infix.insert(l, op+'$')
+                if l == 0 or (infix[l-1] not in functions.keys() and (type(infix[l-1])==str and '$' not in infix[l-1])):
+                    infix.insert(l, op+'$')
+                else:
+                    infix.insert(l+1, '(')
+                    infix.insert(l+1, op+'$')
                 l_go = False
                 r += 1
-                places = pas_posities_aan(places, l)
             elif infix[l] == ')':
                 l = skip_haakjes(infix, l)
 
             if r >= len(infix):
                 infix.append(')')
                 r_go = False
-                places = pas_posities_aan(places, r)
             elif str(infix[r]) in operators and operators[infix[r]]['prec'] <= operators[op]['prec']:
                 infix.insert(r, ')')
                 r_go = False
-                places = pas_posities_aan(places, r)
             elif infix[r] == ')':
                 r_go = False
-                places = pas_posities_aan(places, r)
             elif infix[r] == '(':
                 r = skip_haakjes(infix, r)
-            
+    print(''.join([str(i) for i in infix]))
+    # dollartekens weghalen        
     for i, thing in enumerate(infix):
         if type(thing) == str:
             infix[i] = thing.replace('$', '')
                 
     return infix
 
+
 def main():
-    infx = ['(', '(', 4, '-', 5, ')', '^', 2, ')', '/', 6]
+    infx = ['max', '(', '(', 4, '-', 5, ')', ',', 2,')']
     print(in_to_tree(infx))
 
 if __name__ == '__main__':
