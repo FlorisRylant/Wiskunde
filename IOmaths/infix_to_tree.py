75dcc125549750166ddd9b4b0fd499adde585106
FlorisRylant <florisrylant@gmail.com>
2025-11-20 18:43:15 +0100

    infix_to_tree-algoritme werkt nu

--- IOmaths/infix_to_tree.py
+++ IOmaths/infix_to_tree.py
@@ -14,9 +14,14 @@ def skip_haakjes(lijst, startpos):
 
     if increment == 1:
         i -= 1
-        
+
     return i
 
+def pas_posities_aan(lijst, i, keer=1):
+    for l in lijst:
+        if l >= i:
+            l += keer
+    return lijst
 
 def in_to_tree(infix): # infix = lijst met in volgorde de delen van de uitdrukking (bv [4, '+', 'x'])
     special_chars = special.union(functions, operators)
@@ -30,13 +35,12 @@ def in_to_tree(infix): # infix = lijst met in volgorde de delen van de uitdrukki
             places.append(i)
     offset = 0
 
-    while len(places) != 0: # 'bubbelt' elke operator een voor een naar de juiste plek
-        l = places.pop(0) + offset # startpositie van die operator
-        op = infix[l]
+    for op in ops: # 'bubbelt' elke operator een voor een naar de juiste plek
+        l = infix.index(op) # startpositie van die operator
         infix[l] = ','
         r = l # rechter-'bubbel'
         l_go = True
-        r_go = False
+        r_go = True
         while l_go or r_go:
             l -= int(l_go) # naar buiten bubbelen als ze nog niet geblokkeerd zijn
             r += int(r_go)
@@ -44,33 +48,44 @@ def in_to_tree(infix): # infix = lijst met in volgorde de delen van de uitdrukki
                 infix = [op+'$', '('] + infix
                 l_go = False
                 r += 2
-                offset += 2
+                places = pas_posities_aan(places, l, 2)
             elif str(infix[l]) in operators and operators[infix[l]]['prec'] <= operators[op]['prec']:
                 infix.insert(l+1, '(')
                 infix.insert(l+1, op+'$')
                 l_go = False
                 r += 2
-                offset += 2
+                places = pas_posities_aan(places, l, 2)
             elif infix[l] == '(':
                 infix.insert(l, op+'$')
                 l_go = False
                 r += 1
-                offset += 1
+                places = pas_posities_aan(places, l)
             elif infix[l] == ')':
-                i = l # bevriest tijdelijk het starthaakje
-                l -= 1
-                while infix[l:i+1].count('(') != infix[l:i+1].count(')'): # blijft gaan tot hij bij het haakje sluiten zit
-                    l -= 1
+                l = skip_haakjes(infix, l)
+
+            if r >= len(infix):
+                infix.append(')')
+                r_go = False
+                places = pas_posities_aan(places, r)
+            elif str(infix[r]) in operators and operators[infix[r]]['prec'] <= operators[op]['prec']:
+                infix.insert(r, ')')
+                r_go = False
+                places = pas_posities_aan(places, r)
+            elif infix[r] == ')':
+                r_go = False
+                places = pas_posities_aan(places, r)
+            elif infix[r] == '(':
+                r = skip_haakjes(infix, r)
             
-
+    for i, thing in enumerate(infix):
+        if type(thing) == str:
+            infix[i] = thing.replace('$', '')
                 
-    print(infix)
-
+    return infix
 
 def main():
     infx = ['(', '(', 4, '-', 5, ')', '^', 2, ')', '/', 6]
-    #in_to_tree(infx)
-    print(skip_haakjes(infx, 0))
+    print(in_to_tree(infx))
 
 if __name__ == '__main__':
     main()
