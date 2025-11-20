07dad6d37d23597d730d41cff55865170b8508a4
FlorisRylant <florisrylant@gmail.com>
2025-11-20 19:15:27 +0100

    Nog wat opruimwerk en kleine verbeteringen

--- IOmaths/infix_to_tree.py
+++ IOmaths/infix_to_tree.py
@@ -8,34 +8,21 @@ def skip_haakjes(lijst, startpos):
     else:
         increment = 1
         i = startpos + 1
-
     while lijst[min(i, startpos):max(i, startpos)].count(')') != lijst[min(i, startpos):max(i, startpos)].count('('):
         i += increment
-
     if increment == 1:
         i -= 1
-
     return i
 
-def pas_posities_aan(lijst, i, keer=1):
-    for l in lijst:
-        if l >= i:
-            l += keer
-    return lijst
-
 def in_to_tree(infix): # infix = lijst met in volgorde de delen van de uitdrukking (bv [4, '+', 'x'])
-    special_chars = special.union(functions, operators)
-    ops = []
-    for op in '-+*/^': # maakt een lijst met alle operatoren in infix
-        ops.extend([op for _ in range(infix.count(op))])
+    infix = [str(i) for i in infix]
+    ops = [i for i in infix if i in '+-/*^'] # maakt een lijst met alle operatoren in infix
     
     for op in ops: # 'bubbelt' elke operator een voor een naar de juiste plek
-        print(''.join([str(i) for i in infix]))
-        l = infix.index(op) # startpositie van die operator
+        r = infix.index(op) # startpositie van die operator
+        l = r
         infix[l] = ','
-        r = l # rechter-'bubbel'
-        l_go = True
-        r_go = True
+        l_go, r_go = True, True
         while l_go or r_go:
             l -= int(l_go) # naar buiten bubbelen als ze nog niet geblokkeerd zijn
             r += int(r_go)
@@ -69,14 +56,9 @@ def in_to_tree(infix): # infix = lijst met in volgorde de delen van de uitdrukki
                 r_go = False
             elif infix[r] == '(':
                 r = skip_haakjes(infix, r)
-    print(''.join([str(i) for i in infix]))
-    # dollartekens weghalen        
-    for i, thing in enumerate(infix):
-        if type(thing) == str:
-            infix[i] = thing.replace('$', '')
-                
+    # dollartekens weghalen en output teruggeven
+    infix = '$'.join(infix).replace('$$', '$').split('$')   
     return infix
-
 
 def main():
     infx = ['max', '(', '(', 4, '-', 5, ')', ',', 2,')']
